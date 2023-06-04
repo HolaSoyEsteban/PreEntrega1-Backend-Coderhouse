f0e44eca4936a18278cf21df0bf4d48dab04158e
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-06-04 01:18:36 -0300

    Get, Post y Delete de Productos funcionando

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 import express from 'express'
+import fs from 'fs'
+import productsRouter from './routers/products.Router.js'
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
+const PORT = 8080;
+
+ 
+//---------------------------------------------------------------------------------------------------
+
+// console.log("----------------------------- Arranca el programa ------------------------------------")
+
+// const cantidadDeProductosQueQuieroTener = 5 // cantidad de productos que quiero que se agreguen al archivo productos.json
+
+// elimina el archivo productos.json si es que ya existe
+// await fs.promises.unlink('./src/productos.json')
+
+// crea y agrega la cantidad de productos deseada al archivo productos.json
+// for (let i = 1; i <= cantidadDeProductosQueQuieroTener; i++) {
+//     await productManager.addProduct(`Producto ${i}`, `Este es el producto ${i}`, 200, 'Sin imagen', `abc${i}`, 25) // agrega un producto
+// }
+
+// console.log(await productManager.getProducts()) // muestra el array con los productos agregados
+
+// -----------------------------------------------------------------------------------------------------
+app.use('/api/products', productsRouter)
+
+
+app.listen(PORT, () => console.log('server up'))
---
+++ src/routers/products.Router.js
@@ -0,0 +1,111 @@
+import { Router } from "express";
+import fs from 'fs';
+
+const router = Router();
+
+const filePathProducts = './src/productos.json';
+
+router.get('/', async (req, res) => {
+    console.log('¡Solicitud recibida!');
+    const limit = req.query.limit;
+      try {
+        const data = await fs.promises.readFile(filePathProducts, 'utf-8');
+        const products = JSON.parse(data);
+        if (!limit) {
+          res.json({ products });
+        } else {
+          const productsLimit = products.slice(0, limit);
+          res.json({ products: productsLimit });
+        }
+      } catch (error) {
+        console.log('Error al leer el archivo:', error);
+        res.json({ error: 'Error al leer el archivo' });
+      }
+  });
+
+router.get('/:pid', async (req, res) => {
+    const id = req.params.pid;
+    try {
+      const data = await fs.promises.readFile(filePathProducts, 'utf-8');
+      const products = JSON.parse(data);
+      const product = products.find((product) => product.id == id);
+      if (product) {
+        res.json(product);
+      } else {
+        res.json({ error: 'Producto no encontrado' });
+      }
+    } catch (error) {
+      console.log('Error al leer el archivo:', error);
+      res.json({ error: 'Error al leer el archivo' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const { title, description, code, price, stock, category, thumbnail } = req.body;
+  
+      // Validar que se proporcionen los campos obligatorios
+      if (!title || !description || !code || !price || !stock || !category) {
+        res.status(400).json({ error: 'Faltan campos obligatorios' });
+        return;
+      }
+  
+      const data = await fs.promises.readFile(filePathProducts, 'utf-8');
+      const products = JSON.parse(data);
+  
+      // Verificar si el producto ya existe por el código
+      const existingProduct = products.find((product) => product.code === code);
+      if (existingProduct) {
+        res.status(400).json({ error: 'El producto ya existe' });
+        return;
+      }
+  
+      // Generar un nuevo ID
+      const newProductId = products.length === 0 ? 1 : products[products.length - 1].id + 1;
+  
+      const newProduct = {
+        id: newProductId,
+        title,
+        description,
+        code,
+        price,
+        status: true,
+        stock,
+        category,
+        thumbnails: Array.isArray(thumbnail) ? thumbnail : [thumbnail], // Se asigna el array de thumbnails o se crea un array con el valor único
+      };
+  
+      products.push(newProduct);
+  
+      await fs.promises.writeFile(filePathProducts, JSON.stringify(products, null, 2));
+  
+      res.status(201).json(newProduct);
+    } catch (error) {
+      console.log('Error al leer/escribir el archivo:', error);
+      res.status(500).json({ error: 'Error en el servidor' });
+    }
+});
+
+router.delete('/:pid', async (req, res) => {
+    const id = await req.params.pid;
+    try {
+        const data = await fs.promises.readFile(filePathProducts, 'utf-8');
+        const products = JSON.parse(data);
+        const productIndex = products.findIndex((product) => product.id == id);
+        if (productIndex != -1) {
+            products.splice(productIndex, 1);
+            await fs.promises.writeFile(
+            filePathProducts,
+            JSON.stringify(products, null, 2)
+            );
+            res.json({ message: 'Producto eliminado' });
+        } else {
+            res.json({ error: 'Producto no encontrado' });
+        }
+    } catch (error) {
+        console.log('Error al leer el archivo:', error);
+        res.json({ error: 'Error al leer el archivo' });
+    }
+});
+
+export default router; 
